320db34786cbe05355c56c3ee5fe31161cf7a055
DorzelRamos <dorzel.ramos@gmail.com>
2022-12-14 14:03:55 -0600

    big update

---
+++ js/app2.js
@@ -0,0 +1,100 @@
+$(function(){
+
+  let graph2 = document.getElementById("Graph2");
+
+  console.log(graph2);
+  let myChart = echarts.init(graph2, "dark");
+  let option;
+  const gaugeData = [
+      {
+        value: 20,
+        title: {
+          offsetCenter: ["0%", "0%"],
+        },
+        detail: {
+          valueAnimation: true,
+          offsetCenter: ["0%", "0%"],
+        },
+      },
+    ];
+    option = {
+
+      series: {
+        type: "gauge",
+        startAngle: 90,
+        endAngle: -270,
+        max: 69,
+        min: 0,
+        backgroundColor: "#F00",
+        itemStyle: {
+          backgroundColor: "#F00",
+        },
+        pointer: {
+          show: false,
+        },
+        progress: {
+          show: true,
+          overlap: false,
+          roundCap: true,
+          clip: false,
+        
+          itemStyle: {
+            backgroundColor: "#F00",
+            color: { 
+              backgroundColor: "#F00",
+              type: "linear",
+              x: 0,
+              y: 1,
+              x2: 1,
+              y2: 1,
+              colorStops: [
+                {
+                  offset: 0,
+                  color: "rgb(223, 55, 190)",
+                }, 
+                {
+                  offset: 1,
+                  color: "rgb(106, 64, 204)",
+                },
+              ],
+
+            },
+          },
+        },
+        axisLine: {
+          lineStyle: {
+            width: 20,
+          },
+          itemStyle: {
+            backgroundColor: "#F00"
+          }
+        },
+        splitLine: {
+          show: false,
+        },
+        axisTick: {
+          show: false,
+        },
+        axisLabel: {
+          show: false,
+
+        },
+        data: gaugeData,
+
+        detail: {
+          width: 50,
+          height: 14,
+          color: "#FFFFFF",
+          fontSize: 32,
+          valueAnimation: true,
+          formatter: "{value}",
+          
+        },
+      },
+    };
+    
+    option && myChart.setOption(option);
+
+
+
+});
